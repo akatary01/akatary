cb9a391053d7a30c4bc553c78a85eed46678ba2b
Ahmed Katary <akatary01@gmail.com>
2025-04-23 05:33:42 +0000

    setting up code skeleton

--- src/landing/index.tsx
+++ src/landing/index.tsx
@@ -7,10 +7,12 @@ export default function Landing({...props}: LandingProps) {
     return (
         <div {...props}>
             {/* Left art gallery */}
-
+            <div></div>
             {/* Book */}
+            <div></div>
 
             {/* Right art gallery */}
+            <div></div>
         </div>
     )
 }
