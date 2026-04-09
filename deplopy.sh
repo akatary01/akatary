a839a8b49bc8f77249be7916f5dc5e2a3d20cbef
Ahmed Katary <akatary01@gmail.com>
2026-04-09 02:07:03 +0000

    deploy.sh fix name

--- deplopy.sh
+++
@@ -1,8 +0,0 @@
-sudo rm -rf /var/www/akatary_2.0
-sudo mkdir /var/www/akatary_2.0
-
-shopt -s extglob
-sudo cp -r !(api) /var/www/akatary_2.0/
-
-sudo systemctl restart apache2
-sudo systemctl restart akatary_api.service
