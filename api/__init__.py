bc549d93e2eee7c9d026ae2e97ae99ec37b2c332
Ahmed Katary <akatary01@gmail.com>
2026-07-16 03:01:08 +0000

    adding /mail/send endpoint

--- api/__init__.py
+++ api/__init__.py
@@ -15,7 +15,7 @@ api.add_middleware(
 )
 
 @api.get("/mail/contact")
-async def send_mail(request: ContactRequest = Depends()) -> None:
+async def contact(request: ContactRequest = Depends()) -> None:
     smtp = next((secret for secret in secrets.gmail_smtp if secret.email == request.fromEmail), None)
     if smtp is None:
         raise HTTPException(status_code=400, detail=f"No SMTP credentials configured for {request.fromEmail}")
@@ -30,4 +30,20 @@ async def send_mail(request: ContactRequest = Depends()) -> None:
         server.starttls()
         server.login(smtp.email, smtp.password)
         server.sendmail(smtp.email, request.reciepientEmails.split(","), msg.as_string())
-    
+
+@api.get("/mail/send")
+async def send_mail(request: ContactRequest = Depends()) -> None:
+    smtp = next((secret for secret in secrets.gmail_smtp if secret.email == request.fromEmail), None)
+    if smtp is None:
+        raise HTTPException(status_code=400, detail=f"No SMTP credentials configured for {request.fromEmail}")
+
+    msg = MIMEText(f"{request.message}")
+
+    msg["From"] = smtp.email
+    msg["Subject"] = request.subject
+    msg["To"] = request.reciepientEmails   
+
+    with smtplib.SMTP(smtp.server, smtp.port) as server:
+        server.starttls()
+        server.login(smtp.email, smtp.password)
+        server.sendmail(smtp.email, request.reciepientEmails.split(","), msg.as_string())
